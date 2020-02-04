3209861dca26364e7621303667ee911532b35301
mdubz44 <matthewwisniewski245@gmail.com>
2020-02-03 21:56:46 -0500

    TASK: Added Subtraction function and test.

--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -13,7 +13,13 @@ class MyTestCase(unittest.TestCase):
     def test_calculator_addition(self):
         calculator = Calculator()
         result = calculator.addition(1, 2)
-        self.assertEquals(3, result)
+        self.assertEqual(3, result)
+
+    def test_calculator_subtraction(self):
+        calculator = Calculator()
+        result = calculator.subtraction(2, 1)
+        self.assertEqual(1, result)
+
 
 
 if __name__ == '__main__':
