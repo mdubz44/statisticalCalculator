0c69c37f4d3dc3307815ec4237152603c1399571
mdubz44 <matthewwisniewski245@gmail.com>
2020-03-03 13:04:52 -0500

    TASK: Added Select random number from list W/o seed

--- Tests/test_Random.py
+++ Tests/test_Random.py
@@ -4,6 +4,7 @@ from RandomGenerator.randNumWoSeed import RandNumWoSeed
 from RandomGenerator.randNumWSeed import RandNumWSeed
 from RandomGenerator.randomListGen import RandomListGen
 from RandomGenerator.randomPick import RandomPick
+from RandomGenerator.randomNumsFromList import RandomNumsFromList
 
 class randomTest:
 
@@ -21,3 +22,7 @@ class randomTest:
     def test_RandomGenerator_RandomPick(self):
         alist= [1, 2, 3, 4, 5]
         return Pick(alist)
+
+    def test_RandomGenerator_RandomFromListNoSeed(self):
+        alist = [1,2,3,4,5,6]
+        return NumsFromListNoSeed(alist)
